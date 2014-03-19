48e3be9508175c7af8d0b0bd1c1b61488e29c33d
yann300 <yann.levreau@gmail.com>
2014-03-19 08:51:19 +0100

    commit

--- gk/app/helpers/search_result_helper.rb
+++ gk/app/helpers/search_result_helper.rb
@@ -6,22 +6,25 @@ require "httpclient"
   def Search(searchString)
 	client = HTTPClient.new
 	hightlight = '"highlight" : { "pre_tags" : ["<b>"], "post_tags" : ["</b>"], "fields" : { "content" : {} } }'
+	size = '"from" : 0, "size" : 50'
+	sort = '"sort" : [{ "post_date" : {"order" : "dsc", "ignore_unmapped" : true} },"_score"]'
 
-	query = '{"query": {"query_string": { "query": "' + searchString + '" } }, ' + hightlight + '  }'
+
+	query = '{ ' + sort + ', ' + size + ', "query": { "query_string": { "query": "' + searchString + '" } }, ' + hightlight + '  }'
 
 
 	res = client.post('http://91.121.25.98:9200/_search', query)
-logger = Logger.new(STDOUT)
-logger.debug res.body
+
 	parsed = JSON.parse(res.body)
 
 	resultsA = []
 
-	
-	logger.debug parsed["took"]
-	
 	parsed["hits"]["hits"].each do|item|
-  		resultsA.push(SearchResult.new(item["_source"]["url"], item["_source"]["title"], "", item["highlight"]["content"]))
+		hightlight = ''
+		if (item["highlight"] != nil)
+			hightlight = item["highlight"]["content"]
+		end
+  		resultsA.push(SearchResult.new(item["_source"]["url"], item["_source"]["title"], "", hightlight))
 	end
 	
 	return SearchResults.new(resultsA)
